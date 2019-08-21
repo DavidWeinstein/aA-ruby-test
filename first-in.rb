2a8891c9a5da6448a9cb8585b2cd9c09fc46da8d
David Weinstein <weinstein.dev@gmail.com>
2019-08-20 22:01:18 -0400

    Update first-in.rb

--- first-in.rb
+++ first-in.rb
@@ -1,13 +1,11 @@
 # Write a method first_in_array that takes in an array and two elements, the method should return the element that appears earlier in the array.
 
 def first_in_array(arr, ele1, ele2)
-    first = ""
     if arr.index(ele1) < arr.index(ele2)
-        first = ele1
+        return ele1
     else
-        first = ele2
+        return ele2
     end
-    return first
 end
 
 puts first_in_array(["a", "b", "c", "d"], "d", "b"); # => "b"
